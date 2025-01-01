27455531777224e0e82f01595b8ce42fcaf66fc1
King Lawrence Caubalejo <kinglawrencecaubalejo@Lawrence.local>
2025-01-01 18:57:33 +0800

    feat: add camper and routing

--- src/app/views/base/campers/add-camper/add-camper.component.ts
+++ src/app/views/base/campers/add-camper/add-camper.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { 
+  ContainerComponent,
+  RowComponent,
+  ColComponent,
+  CardComponent, 
+  CardHeaderComponent, 
+  CardBodyComponent,
+  FormCheckComponent,
+  FormControlDirective,
+  FormSelectDirective,
+  ButtonDirective 
+} from '@coreui/angular';
 
 @Component({
   selector: 'app-add-camper',
-  imports: [],
+  imports: [
+    ContainerComponent,
+    RowComponent,
+    ColComponent,
+    CardComponent, 
+    CardHeaderComponent, 
+    CardBodyComponent,
+    FormCheckComponent,
+    FormControlDirective,
+    FormSelectDirective,
+    ButtonDirective 
+  ],
   templateUrl: './add-camper.component.html',
   styleUrl: './add-camper.component.scss'
 })
