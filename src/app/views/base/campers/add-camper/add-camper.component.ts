f3e2493684ed82dfccc7ece7847454a2a61c4658
King Lawrence Caubalejo <kinglawrencecaubalejo@Lawrence.local>
2025-01-01 18:20:01 +0800

    feat: add new camper module

---
+++ src/app/views/base/campers/add-camper/add-camper.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-camper',
+  imports: [],
+  templateUrl: './add-camper.component.html',
+  styleUrl: './add-camper.component.scss'
+})
+export class AddCamperComponent {
+
+}
