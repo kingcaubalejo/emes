fa2f733840fe02abad4ff8457ce5786a3e1a6958
King Lawrence Caubalejo <kinglawrencecaubalejo@Lawrence.local>
2025-01-02 01:05:09 +0800

    feat: church component

--- src/app/views/groups/churches/churches.component.ts
+++ src/app/views/groups/churches/churches.component.ts
@@ -19,7 +19,41 @@ import {
 })
 export class ChurchesComponent {
 
-  states = ['normal', 'active', 'disabled'];
-  colors = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'];
+  churches: Array<{row: number , content: Object[] }> = [];
+
+  constructor() {
+    this.churches = [
+      {
+        row: 1,
+        content: [
+          { church: 1, church_name: 'Capstone Message Believers Church', church_members: 50 },
+          { church: 2, church_name: 'Capstone Message Believers Church', church_members: 50 },
+          { church: 3, church_name: 'Capstone Message Believers Church', church_members: 50 }, 
+          { church: 4, church_name: 'Capstone Message Believers Church', church_members: 50 }
+        ]
+      },{
+        row: 2,
+        content: [
+          { church: 5, church_name: 'Capstone Message Believers Church', church_members: 50 },
+          { church: 6, church_name: 'Capstone Message Believers Church', church_members: 50 },
+          { church: 7, church_name: 'Capstone Message Believers Church', church_members: 50 }, 
+          { church: 8, church_name: 'Capstone Message Believers Church', church_members: 50 }
+        ]
+      },{
+        row: 3,
+        content: [
+          { church: 9, church_name: 'Capstone Message Believers Church', church_members: 50 },
+          { church: 10, church_name: 'Capstone Message Believers Church', church_members: 50 },
+          { church: 11, church_name: 'Capstone Message Believers Church', church_members: 50 }, 
+          { church: 12, church_name: 'Capstone Message Believers Church', church_members: 50 }
+        ]
+      },{
+        row: 4,
+        content: [
+          { church: 13, church_name: 'Capstone Message Believers Church', church_members: 50 },
+        ]
+      }
+    ];
+  }
 
 }
